91a107b92a3d54ce453d9560a7ee4570760de12e
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-25 21:03:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,13 +30,14 @@ def adding
                   "oil paintings" => 3
                  }
                  shipping_manifest["muskets"]=2
+                 shipping_manifest["gun powder"]=4
   # add 2 muskets to the shipping_manifest hash below
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest
 
   # return the shipping_manifest hash below
 
